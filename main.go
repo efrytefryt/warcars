295c54bcd243a9abc992c5747d85c921b6597e28
Efryt <efryt@o2.pl>
2018-07-30 14:04:40 +0200

    sdded few structs

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"github.com/google/uuid"
+)
+
+type player struct {
+	id uuid.UUID
+}
+
+func (player) load(id uuid.UUID) player {
+
+	p := player{id: id}
+
+	return p
+}
+
+type inventory struct {
+	parts []string
+}
+
 func main() {
 
 }
